c82d84a094a92041ba04d4cba1664a8c6c4e9838
Maxim <mainmaxxx@gmail.com>
2019-05-02 16:36:22 +0300

    Version 1.5 path is completed

--- ymapServerJS/public/ymapSrc.js
+++ ymapServerJS/public/ymapSrc.js
@@ -1,5 +1,7 @@
 "use strict"
 
+//import { pipeline } from "stream";
+
 //import { isObject } from "util";
 
 /**
@@ -24,98 +26,99 @@ function init () {
     }, {
         searchControlProvider: 'yandex#search'  
     });
-    
-   
-    let arrayOfCars = [];
-    for (let i = 0, len = arrayOfCars.length; i < len; i++) {
-        // Вычисление длительности и времени проезда до машины и возвращение в массив
+    acceptRequest();//Принимаем запрос от сервера с данными и начинаем их обработку
+}
+function getPath(latitudeYourGeolocation, longitudeYourGeolocation, latitudeCarGeolocation, longitudeCarGeolocation, isAuto){
+    // Геолокация пользователя при Auto = false, и геолокации пользователя в машине при Auto = true
+    var yourGeolocation = [latitudeYourGeolocation, longitudeYourGeolocation]; 
+    // Геолокация машины при Auto = false (машина как искомый объект), при Auto = true (пункт назначения)
+    var needObjectGeolocation = [latitudeCarGeolocation, longitudeCarGeolocation];
+    let movement;//Способ передвижение (на автомобиле или пешком)
+    if (isAuto) {
+        var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([yourGeolocation, needObjectGeolocation], {
+            routingMode: 'auto',
+            avoidTrafficJams: true
+        });
+        var multiRouteView = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
+            // Внешний вид линии маршрута.
+        routeStrokeWidth: 2,
+        routeStrokeColor: "#000088",
+        routeActiveStrokeWidth: 6,
+        routeActiveStrokeColor: "#00BFFF",
+        });
+
+    } else {
+        var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([yourGeolocation, needObjectGeolocation], {
+            routingMode: 'pedestrian',
+            
+        });
+        var multiRouteView = new ymaps.multiRouter.MultiRoute(multiRouteModel ,{ 
+        // Внешний вид линии пешеходного маршрута.
+        //routeActivePedestrianSegmentStrokeStyle: "solid",
+        routeActivePedestrianSegmentStrokeColor: "#FFA500"
+        });
     }
-    sendRequest();
-    sendXMLHttpRequest(getHumanPath(55.76, 37.64, 55.74, 37.62));
-    sendXMLHttpRequest(getСarPath(55.74, 37.62, 55.77, 37.61));
+    myMap.geoObjects.add(multiRouteView);  
+    // Подписываемся на события модели мультимаршрута. 
+    let asyncFuntion = (multiRouteView, isAuto) => {
+        return new Promise((resolve)=> {
+                multiRouteView.model.events
+                    .add("requestsuccess", function (event) {
+                        var routes = event.get("target")
+                            .getRoutes();
+                        if (routes.length > 0)
+                        {
+                            console.log("Найдено маршрутов: " + routes.length);
+                            for (var i = 0, l = routes.length; i < l; i++) {
+                                console.log("Длина маршрута " + (i + 1) + ": " + routes[i]
+                                    .properties.get("distance")
+                                    .text);
+                                console.log(routes[i].properties.get('duration').text);
+                                }
+                            let data = {};
+                            data.MultiRoute = multiRouteView;
+                            data.movement = isAuto;
+                            data.arrRoutes = routes;
+                            resolve(data);
+                        }
+                    })
+                    .add("requestfail", function (event) {
+                        console.log("Ошибка: " + event.get("error")
+                            .message);
+                    });  
+            });
+            
+        }
+        return asyncFuntion(multiRouteView, isAuto).then(resolve => {
+            return getRouteOrError(resolve);   
+            })
+    }
 
-}
-
-
-function getСarPath(latitudeYourGeolocation, longitudeYourGeolocation, latitudeCarGeolocation, longitudeCarGeolocation)
-{
-    var needCarRoute = {};
-    var yourGeolocation = [latitudeYourGeolocation, longitudeYourGeolocation];
-    var carGeolocation = [latitudeCarGeolocation, longitudeCarGeolocation];
-    var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([yourGeolocation, carGeolocation], {
-    routingMode: 'auto',
-    avoidTrafficJams: true
-});
-// Создаем отображение мультимаршрута на основе модели.
-var multiRouteView = new ymaps.multiRouter.MultiRoute(multiRouteModel);
-myMap.geoObjects.add(multiRouteView);
-// Подписываемся на события модели мультимаршрута.
-multiRouteView.model.events
-    .add("requestsuccess", function (event) {
-        var routes = event.get("target")
-            .getRoutes();
-        needCarRoute = getRouteOrError(routes, true);
-        console.log("Найдено маршрутов: " + routes.length);
-        for (var i = 0, l = routes.length; i < l; i++) {
-            console.log("Длина маршрута " + (i + 1) + ": " + routes[i].properties.get("distance").text);
-            console.log(routes[i].properties.get('durationInTraffic').text);
-            }     
-    })
-    .add("requestfail", function (event) {
-        console.log("Ошибка: " + event.get("error")
-            .message);
-    });
-    return needCarRoute;
-}
-
-function getHumanPath(latitudeYourGeolocation, longitudeYourGeolocation, latitudeCarGeolocation, longitudeCarGeolocation){
-    var needHumanRoute = {};
-    var yourGeolocation = [latitudeYourGeolocation, longitudeYourGeolocation];
-    var carGeolocation = [latitudeCarGeolocation, longitudeCarGeolocation];
-    var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([yourGeolocation, carGeolocation], {
-    routingMode: 'pedestrian'
-});
-// Создаем отображение мультимаршрута на основе модели.
-var multiRouteView = new ymaps.multiRouter.MultiRoute(multiRouteModel);
-myMap.geoObjects.add(multiRouteView);
-// Подписываемся на события модели мультимаршрута.
-multiRouteView.model.events
-    .add("requestsuccess", function (event) {
-        var routes = event.get("target")
-            .getRoutes();
-        needHumanRoute = getRouteOrError(routes, false);
-        console.log("Найдено маршрутов: " + routes.length);
-        for (var i = 0, l = routes.length; i < l; i++) {
-            console.log("Длина маршрута " + (i + 1) + ": " + routes[i]
-                .properties.get("distance")
-                .text);
-            console.log(routes[i].properties.get('duration').text);
-            }   
-    })
-    .add("requestfail", function (event) {
-        console.log("Ошибка: " + event.get("error")
-            .message);
-    });
-    return needHumanRoute;
-}
-function getRouteOrError(routes, isAuto)
+function getRouteOrError(object)
 {
     let movement = 'duration'; // Передвижение пешком
-    if (isAuto) 
+    if (object.movement) 
         movement = 'durationInTraffic'; // Передвижение на автомобиле
-    let needRoute = {};
     let numberOfRoute = -1;
-    switch (routes.length) {
+    switch (object.arrRoutes.length) {
         case 0:
             throw "No path";
         case 1:numberOfRoute = 0;
             break;
-        default: numberOfRoute = getMinimum(routes, movement);
+        default: numberOfRoute = getMinimum(object.arrRoutes, movement);
     }
-    needRoute.duration = routes[numberOfRoute].properties.get(movement).value/60; // Длительность В минутах
-    needRoute.distance = routes[numberOfRoute].properties.get(movement).value/1000; // Дальность в километрах
-    return needRoute;
+    if (object.movement) {
+        object.durationCar = object.arrRoutes[numberOfRoute].properties.get(movement).value/60; // Длительность В минутах
+        object.distanceCar = object.arrRoutes[numberOfRoute].properties.get('distance').value/1000; // Дальность в километрах     
+    } else {
+        object.durationHuman = object.arrRoutes[numberOfRoute].properties.get(movement).value/60; // Длительность В минутах
+        object.distanceHuman = object.arrRoutes[numberOfRoute].properties.get('distance').value; // Дальность в метрах
+    }
+    delete object.movement;
+    delete object.arrRoutes; 
+    return object;   
 }
+
 function getMinimum(routes, movement) // 'duration'  'durationInTraffic'
 {
     
@@ -124,7 +127,7 @@ function getMinimum(routes, movement) // 'duration'  'durationInTraffic'
     let numberOfRoute = 0;
         for (let i = 1, len = routes.length; i < len; i++) {
             let currentElement = routes[i].properties.get(movement).value/60
-            if (min < currentElement) {
+            if (min > currentElement) {
                 min = currentElement;
                 numberOfRoute = i;
         }
@@ -148,47 +151,39 @@ function sendXMLHttpRequest(data)
         else{
             console.log('error');
         }
-    };
-    
+    };   
 }
 
-function sendRequest(){
+function acceptRequest(){
     console.log('get started Fetch ');
     fetch('/arrayOfCars')       
     .then(responce => {return responce.json()})
-    .then(responce => getArrayOfRoutes(responce))
+    .then(responce => addTimePathOnArrayOfCars(responce))
     .catch(error => console.log(error));
 }
 // latitude
 // longitude
-function getArrayOfRoutes(arrayOfCars){
-    arrayOfCars = isJson(arrayOfCars);
+function addTimePathOnArrayOfCars(arrayOfCars){
+    let latitude // Широта
+    let longitude // Долгота
     // В цикле запускать функции расстояния для каждого элемента массива и и добавлять результаты в массив
     for (let i = 0, len = arrayOfCars.length; i < len; i++) {
         //console.log(isJson(JSON.parse(JSON.stringify(arrayOfCars[i]))));
         latitude = Number(arrayOfCars[i].latitude);
         longitude = Number(arrayOfCars[i].longitude);
-        arrayOfCars[i].timeToCar = getHumanPath(37.480220, 55.669940, latitude, longitude);
-        arrayOfCars[i].timeToDestination = getСarPath(37.480220, 55.669940, latitude, longitude);
+        let promiseHuman = new Promise((resolve)=>
+        {
+            // Получим данные о пути от пешехода до машины
+            resolve(getPath(55.76, 37.64, latitude, longitude, false));
+        })
+        .then(resolve => arrayOfCars[i].routeHuman = resolve)
+        .then(console.log(arrayOfCars[i]));
+        let promiseCar = new Promise((resolve)=>
+        {
+            // Получим данные о пути на машине до пункта назначения
+            resolve(getPath(55.76, 37.64, latitude, longitude,true));
+        })
+        .then(resolve => arrayOfCars[i].routeCar = resolve)
+        .then(console.log(arrayOfCars[i]));
     }
-}
-
-function isJson(str) {
-    let result = null;
-    /*
-    str = str.replace(/\\n/g, "\\n")  
-               .replace(/\\'/g, "\\'")
-               .replace(/\\"/g, '\\"')
-               .replace(/\\&/g, "\\&")
-               .replace(/\\r/g, "\\r")
-               .replace(/\\t/g, "\\t")
-               .replace(/\\b/g, "\\b")
-               .replace(/\\f/g, "\\f");
-// remove non-printable and other non-valid JSON chars
-str = str.replace(/[\u0000-\u0019]+/g,""); */
-    try {
-        return JSON.parse(str);
-    } catch (e) {
-        throw "Error with JSON";
-    }  
 }
