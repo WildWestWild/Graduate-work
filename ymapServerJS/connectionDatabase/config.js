b6a21cf30af7d1e2cf60f8a343f24d82d5df1e78
WildWestWild <mainmaxxx@gmail.com>
2019-06-11 12:19:21 +0300

    Vers 2.3

--- ymapServerJS/connectionDatabase/config.js
+++ ymapServerJS/connectionDatabase/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 80,
+    PORT: process.env.PORT || 4222,
     GLOBAL_MONGO_URL:'mongodb+srv://mainmaxxx:$skam1962@cluster0-awhdg.mongodb.net/test?retryWrites=true',
     GLOBAL_URL_SERVER: 'http://176.223.138.38:4222',
     LOCAL_URL: 'http://localhost/'
